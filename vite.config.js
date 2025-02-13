d5cc81ead4ee2fca49619da711e49abdb756a16c
JahanZaib Malik <116706071+Zaib001@users.noreply.github.com>
2025-02-12 21:56:12 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/',  // Keep this for Render
+  server: {
+    historyApiFallback: true,  // Ensures client-side routing works
+  }
+});
